3b07f6b4e4c53661608519d06ff17b371055100e
user <user@EE06>
2020-09-29 09:38:39 +0800

    fix

--- src/lab1/Lab1_2.java
+++ src/lab1/Lab1_2.java
@@ -4,7 +4,7 @@ public class Lab1_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I'm À¹Êo¤ß");
+		System.out.println("I'm Dai shao hsin");
 		System.out.println("My nid is D0593174");
 		System.out.println("This is lab1 hw");
 
